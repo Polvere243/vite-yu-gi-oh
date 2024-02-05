adff115f8e42ace0a1774335def4144caa532b48
Polvere243 <danielercoli@gmail.com>
2024-02-05 20:10:16 +0100

    created a component for the main and gave some style to some elements

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script>
+import AppMain from "./components/AppMain.vue"
 export default {
-  name: "Pokédex"
+  name: "Pokédex",
+  components: { AppMain }
 }
 </script>
 
-<template></template>
+<template>
+  <AppMain />
+</template>
 
 <style>
 /* css reset */
@@ -17,5 +21,13 @@ export default {
 /* colore di sfondo */
 body {
   background-color: rgb(187, 0, 0);
+}
+
+/* generici */
+img {
+  display: block;
+  border-radius: 50%;
+  height: 80px;
+  width: 80px;
 }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "MainApp"
+}
+</script>
+
+<template>
+    <main id="container">
+        <div class="row">
+            <div class="card">
+                <figure>
+                    <img src="" alt="">
+                    <figcaption>
+
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </main>
+</template>
